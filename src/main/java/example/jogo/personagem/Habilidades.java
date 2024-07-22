3c23c22fd27430d168a45b62da1e63d5608459a4
luis-fbs <luis-fbs@outlook.com>
2024-07-21 22:24:44 -0300

    Habilidades

---
+++ src/main/java/example/jogo/personagem/Habilidades.java
@@ -0,0 +1,17 @@
+package example.jogo.personagem;
+
+public class Habilidades {
+    private Boolean furtivo,
+                    veloz,
+                    bonsReflexos,
+                    montarEstrategia,
+                    espionar;
+
+    public Habilidades(Boolean furtivo, Boolean veloz, Boolean bonsReflexos, Boolean montarEstrategia, Boolean espionar) {
+        this.furtivo = furtivo;
+        this.veloz = veloz;
+        this.bonsReflexos = bonsReflexos;
+        this.montarEstrategia = montarEstrategia;
+        this.espionar = espionar;
+    }
+}
